c31271d2cc19332a3ef214ed658ae29860c575ab
Pete Bachant <petebachant@gmail.com>
2016-04-12 07:20:00 -0400

    Remove GUI build script in favor of Makefile

--- gui/build.bat
+++
@@ -1,9 +0,0 @@
-echo Building GUI
-
-call pyuic4 gui\mainwindow.ui > modules\mainwindow.py
-
-echo Building resource file
-
-call pyrcc4 -py3 gui\icons\resources.qrc -o modules\resources_rc.py
-
-echo Done
