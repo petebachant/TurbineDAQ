248c53b72be0c12daee06acaacaed67fa4652c73
Pete Bachant <petebachant@gmail.com>
2024-03-16 06:55:49 -0700

    Simplify example

--- test/run.py
+++ test/run.py
@@ -1,19 +1,13 @@
-# -*- coding: utf-8 -*-
+"""This is a test run script to be placed in an experiment working directory.
 """
-Created on Tue Jan 13 12:47:12 2015
 
-@author: Pete
-
-This is a test run script to be placed in an experiment working directory.
-
-"""
-from __future__ import print_function
 import argparse
 import Modules
 
 parser = argparse.ArgumentParser()
-parser.add_argument("function", help="Function to run in the script",
-                    nargs="?")
+parser.add_argument(
+    "function", help="Function to run in the script", nargs="?"
+)
 parser.add_argument("section", help="Test plan section", nargs="?")
 parser.add_argument("nrun", type=int, help="Run number", nargs="?")
 args = parser.parse_args()
