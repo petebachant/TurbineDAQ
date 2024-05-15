105cd342dbf970d9d89f950dcd1807de86cd13a6
Pete Bachant <petebachant@gmail.com>
2024-05-15 13:16:35 -0400

    Get INF4 data collection working with NTM

--- scripts/collect_inf4_data.py
+++ scripts/collect_inf4_data.py
@@ -20,12 +20,7 @@ if __name__ == "__main__":
     # Note that when we want to connect to both the NTM and EC we will need
     # to change the IP address or port for the EC so they aren't identical
     print("Connecting to the controller")
-    hc = acsc.openCommEthernetTCP("10.0.0.102")
-    target_serial_number = "ECM18038C"
-    actual_serial_number = acsc.getSerialNumber(hc).strip()
-    print("Connected to controller serial number:", actual_serial_number)
-    if actual_serial_number != target_serial_number:
-        raise RuntimeError("Connected to the wrong controller")
+    hc = acsc.openCommEthernetTCP("10.0.0.100")
     # Stop data collection if this failed last time
     try:
         acsc.writeInteger(hc, "collect_data", 0)
@@ -37,10 +32,11 @@ if __name__ == "__main__":
         n_buffer_rows=N_BUFFER_ROWS,
         n_buffer_cols=N_BUFFER_COLS,
     )
-    print("Loading program into buffer 9:\n", prg_txt)
-    acsc.loadBuffer(hc, 9, prg_txt, 6000)
-    print("Running buffer 9")
-    acsc.runBuffer(hc, 9)
+    buffno = 17
+    print(f"Loading program into buffer {buffno}:\n", prg_txt)
+    acsc.loadBuffer(hc, buffno, prg_txt, 6000)
+    print(f"Running buffer {buffno}")
+    acsc.runBuffer(hc, buffno)
     # Collect data for a bit then set collect_data=0 to stop data collection
     normalized_time_vals = []
     ch1_force_vals = []
--- turbinedaq/acsprgs.py
+++ turbinedaq/acsprgs.py
@@ -11,48 +11,50 @@ AFT_TEMPLATE = """
 global int inf4_data(8)(100) ! 8 columns and 100 rows
 global int collect_data
 global real start_time
-local int sample_period_ms = {sample_period_ms}
+local int sample_period_ms
+sample_period_ms = {sample_period_ms}
 global real ch1_force, ch2_force, ch3_force, ch4_force
 global real inf4_data_processed({n_buffer_cols})({n_buffer_rows})
 
-local int subtract_value = 16777215
+local int subtract_value
+subtract_value = 16777215
 local int sign_value
 
-ECIN(ECGETOFFSET("1 Byte In (0)", 1), DI0) ! CH1, Byte 1 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (1)", 1), DI1) ! CH1, Byte 2 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (2)", 1), DI2) ! CH1, Byte 3 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (3)", 1), DI3) ! CH1, Byte 4 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (0)", 4), DI0) ! CH1, Byte 1 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (1)", 4), DI1) ! CH1, Byte 2 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (2)", 4), DI2) ! CH1, Byte 3 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (3)", 4), DI3) ! CH1, Byte 4 in Hi-Res Mode
 
-ECIN(ECGETOFFSET("1 Byte In (4)", 1), DI4) ! CH2, Byte 1 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (5)", 1), DI5) ! CH2, Byte 2 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (6)", 1), DI6) ! CH2, Byte 3 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (7)", 1), DI7) ! CH2, Byte 4 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (4)", 4), DI4) ! CH2, Byte 1 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (5)", 4), DI5) ! CH2, Byte 2 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (6)", 4), DI6) ! CH2, Byte 3 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (7)", 4), DI7) ! CH2, Byte 4 in Hi-Res Mode
 
-ECIN(ECGETOFFSET("1 Byte In (8)", 1), DI8) ! CH3, Byte 1 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (9)", 1), DI9) ! CH3, Byte 2 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (10)", 1), DI10) ! CH3, Byte 3 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (11)", 1), DI11) ! CH3, Byte 4 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (8)", 4), DI8) ! CH3, Byte 1 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (9)", 4), DI9) ! CH3, Byte 2 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (10)", 4), DI10) ! CH3, Byte 3 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (11)", 4), DI11) ! CH3, Byte 4 in Hi-Res Mode
 
-ECIN(ECGETOFFSET("1 Byte In (12)", 1), DI12) ! CH4, Byte 1 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (13)", 1), DI13) ! CH4, Byte 2 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (14)", 1), DI14) ! CH4, Byte 3 in Hi-Res Mode
-ECIN(ECGETOFFSET("1 Byte In (15)", 1), DI15) ! CH4, Byte 4 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (12)", 4), DI12) ! CH4, Byte 1 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (13)", 4), DI13) ! CH4, Byte 2 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (14)", 4), DI14) ! CH4, Byte 3 in Hi-Res Mode
+ECIN(ECGETOFFSET("1 Byte In (15)", 4), DI15) ! CH4, Byte 4 in Hi-Res Mode
 
-ECIN(ECGETOFFSET("1 Byte In (16)", 1), DI16) ! Digital Outputs 1st Byte in Default Mode
-ECIN(ECGETOFFSET("1 Byte In (17)", 1), DI17) ! Digital Outputs 2nd Byte in Default Mode
+ECIN(ECGETOFFSET("1 Byte In (16)", 4), DI16) ! Digital Outputs 1st Byte in Default Mode
+ECIN(ECGETOFFSET("1 Byte In (17)", 4), DI17) ! Digital Outputs 2nd Byte in Default Mode
 
 ! Mapping all digital outputs from INF4 to ASCPL+ variables
 
-ECOUT(ECGETOFFSET("1 Byte Out (0)", 1), DO0) ! Command Register 1st Byte
-ECOUT(ECGETOFFSET("1 Byte Out (1)", 1), DO1) ! Command Register 2nd Byte
+ECOUT(ECGETOFFSET("1 Byte Out (0)", 4), DO0) ! Command Register 1st Byte
+ECOUT(ECGETOFFSET("1 Byte Out (1)", 4), DO1) ! Command Register 2nd Byte
 
-ECOUT(ECGETOFFSET("1 Byte Out (2)", 1), DO2) ! Digital Outputs Command 1st Byte
-ECOUT(ECGETOFFSET("1 Byte Out (3)", 1), DO3) ! Digital Outputs Command 2nd Byte
+ECOUT(ECGETOFFSET("1 Byte Out (2)", 4), DO2) ! Digital Outputs Command 1st Byte
+ECOUT(ECGETOFFSET("1 Byte Out (3)", 4), DO3) ! Digital Outputs Command 2nd Byte
 
-ECOUT(ECGETOFFSET("1 Byte Out (4)", 1), DO4) ! Exchange Register 1st Byte
-ECOUT(ECGETOFFSET("1 Byte Out (5)", 1), DO5) ! Exchange Register 2nd Byte
-ECOUT(ECGETOFFSET("1 Byte Out (6)", 1), DO6) ! Exchange Register 3rd Byte
-ECOUT(ECGETOFFSET("1 Byte Out (7)", 1), DO7) ! Exchange Register 4th Byte
+ECOUT(ECGETOFFSET("1 Byte Out (4)", 4), DO4) ! Exchange Register 1st Byte
+ECOUT(ECGETOFFSET("1 Byte Out (5)", 4), DO5) ! Exchange Register 2nd Byte
+ECOUT(ECGETOFFSET("1 Byte Out (6)", 4), DO6) ! Exchange Register 3rd Byte
+ECOUT(ECGETOFFSET("1 Byte Out (7)", 4), DO7) ! Exchange Register 4th Byte
 
 ! Put into high res mode
 DO1 = 25
@@ -70,7 +72,7 @@ WHILE collect_data
     BLOCK
         ! Compute all force values in the same controller cycle
         ! Channel 1
-        ch1_force = (DI1 << 16) | (DI2 << 8) | DI3
+        ch1_force = (DI1 * POW(2,16)) | (DI2 * POW(2,8)) | DI3
         if DI0
             sign_value = -1
             ch1_force = subtract_value - ch1_force
@@ -83,7 +85,7 @@ WHILE collect_data
         ch1_force = ch1_force * 15.7085561 * sign_value
         
         ! Channel 2
-        ch2_force = (DI5 << 16) | (DI6 << 8) | DI7
+        ch2_force = (DI5 * POW(2,16)) | (DI6 * POW(2,8)) | DI7
         if DI4
             sign_value = -1
             ch2_force = subtract_value - ch2_force
@@ -96,7 +98,7 @@ WHILE collect_data
         ch2_force = ch2_force * 15.7617411 * sign_value
         
         ! Channel 3
-        ch3_force = (DI9 << 16) | (DI10 << 8) | DI11
+        ch3_force = (DI9 * POW(2,16)) | (DI10 * POW(2,8)) | DI11
         if DI8
             sign_value = 1
             ch3_force = subtract_value - ch3_force
@@ -109,7 +111,7 @@ WHILE collect_data
         ch3_force = ch3_force * 390.354011 * sign_value
 
         ! Channel 4
-        ch4_force = (DI13 << 16) | (DI14 << 8) | DI15
+        ch4_force = (DI13 * POW(2,16)) | (DI14 * POW(2,8)) | DI15
         if DI12
             sign_value = -1
             ch4_force = subtract_value - ch4_force
