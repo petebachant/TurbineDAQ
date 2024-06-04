f122248e8af2b6c4a119935acecd256c8637e616
meganandd <mandersen.731@gmail.com>
2024-06-04 14:29:59 -0400

    Return back to normal JERK param.

--- turbinedaq/acsprgs.py
+++ turbinedaq/acsprgs.py
@@ -63,7 +63,7 @@ ptp/e 5, 0
 ACC(5) = 1.0
 DEC(5) = 0.5
 VEL(5) = U
-JERK(5)= ACC(5)*10 * 0.01
+JERK(5)= ACC(5)*10
 
 ! Set modulo on turbine axis (only needed if using simulator)
 ! DISABLE 6
@@ -74,7 +74,7 @@ JERK(5)= ACC(5)*10 * 0.01
 ACC(6) = rpm/tacc
 VEL(6) = rpm
 DEC(6) = ACC(6)
-JERK(6)= ACC(6)*10 * 0.01
+JERK(6)= ACC(6)*10
 
 ! Move turbine to zero if necessary
 if RPOS(6) <> 60 & RPOS(6) <> 0
