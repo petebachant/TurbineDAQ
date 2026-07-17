0d31cbe7f7418026e3f56a6e7debb904a3d2ca61
Megan Andersen <mandersen.731@gmail.com>
2026-07-17 11:42:27 -0400

    Update acsprgs.py
    Troubleshooting zero routine

--- turbinedaq/acsprgs.py
+++ turbinedaq/acsprgs.py
@@ -36,8 +36,8 @@ AFT_TOW_TEMPLATE = """
 ! AUTO-GENERATED -- CHANGES WILL BE OVERWRITTEN
 local real target, tsr, U, rpm, tacc, endpos, tzero, R, position_actual, revs, dist_to_0, remainder
 global real start_time
-global int collect_data, internal_offset
-local int sample_period_ms, revs_int
+global int collect_data
+local int sample_period_ms, revs_int, internal_offset
 sample_period_ms = {sample_period_ms}
 global real ch1_force, ch2_force, ch3_force, ch4_force
 global real aft_data(8)({n_buffer_rows})
@@ -88,11 +88,14 @@ ptp/e 5, target
 HALT(6)
 ACC(5) = 0.3
 VEL(5) = 0.5
-VEL(6) = 10
+ptp/e 5, endpos
 
+VEL(6) = 10
 ! Move instrumented AFT blade to 0-degree position (12:00)
-revs_int = FPOS(6) / 60
-revs = FPOS(6) / 60
+internal_offset = 89370 ! Encoder count when instrumented blade is at 0 degrees
+position_actual = COEREAD/4 (3, 0x6064, 0) - internal_offset
+revs_int = position_actual / 360000
+revs = position_actual / 360000
 if revs_int > revs 
     revs_int = revs_int - 1
 end
@@ -100,7 +103,6 @@ remainder = revs - revs_int
 dist_to_0 = 60 - (60 * remainder) ! 60 = 360 degrees, find relative distance to 0 
 ptp/re 6, dist_to_0 ! Perform PTP(6) before carriage starts moving, move instrumented blade to 0 degree position
 
-ptp/e 5, endpos
 STOPDC
 collect_data = 0
 OUT1.16 = 0
